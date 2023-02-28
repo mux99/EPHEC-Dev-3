3c9bdb90cc828986c1a3a1b109f675f3485833d9
maxime <mux99@live.be>
2023-02-28 15:20:41 +0100

    prepare for new branch structure

--- WEB/src/app/app.module.ts
+++ WEB/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { UserActions } from './userActions/userActions.component';
 import { LandingPage } from './landingPage/landingPage.component';
+import { TimelinePage } from './timelinePage/timelinePage.component';
 import { ProjectSmall } from './projectSmall/projectSmall.component';
 
 @NgModule({
-  declarations: [AppComponent,UserActions,LandingPage,ProjectSmall],
+  declarations: [AppComponent,UserActions,LandingPage,ProjectSmall,LandingPage,TimelinePage],
   imports: [
     BrowserModule,
   ],
---
+++ WEB/src/app/timelinePage/timelinePage.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'timeline-page',
+  templateUrl: './timelinePage.component.html',
+  styleUrls: ['./timelinePage.component.scss']
+})
+
+export class TimelinePage { }
