97596f94ca8e0dbac7de85581328961f2cb90ed4
maxime <mux99@live.be>
2023-03-03 12:54:05 +0100

    start timeline mockup

---
+++ WEB/src/app/timelineEvent/timelineEvent.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'timeline-event',
+  templateUrl: './timelineEvent.component.html',
+  styleUrls: ['./timelineEvent.component.scss']
+})
+
+export class TimelineEvent { }
