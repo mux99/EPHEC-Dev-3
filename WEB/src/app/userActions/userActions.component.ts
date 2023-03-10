ee179c02c0c713ce93f6df9f0752fd4b484588bc
maxime <mux99@live.be>
2023-03-10 11:57:53 +0100

    better landing page still mock-up

--- WEB/src/app/userActions/userActions.component.ts
+++ WEB/src/app/userActions/userActions.component.ts
@@ -7,8 +7,31 @@ import { Component } from '@angular/core';
 })
 
 export class UserActions {
-  onClick() {
-    var panel = document.getElementById("UserActions");
+  setCookie(cname: string, cvalue: string, exdays: number) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  getCookie(cname: string) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+  userIconClick() {
+    let panel = document.getElementById("UserActions");
     if (panel?.classList.contains("visible")) {
       panel.classList.add("hidden");
       panel.classList.remove("visible");
@@ -18,6 +41,24 @@ export class UserActions {
       panel?.classList.remove("hidden");
       panel?.classList.add("visible");
       console.log("test2");
+    }
+  }
+
+  userConnectClick() {
+
+  }
+
+  conncetionCheck() {
+    let username = this.getCookie("username");
+    let connectButton = document.getElementById("userConnect");
+    let userButton = document.getElementById("userIcon");
+    if (username != "") {
+      if (connectButton?.style.display) {
+        connectButton.style.display = "none";
+      }
+      if (userButton?.style.display) {
+        userButton.style.display = "block";
+      }
     }
   }
 }
