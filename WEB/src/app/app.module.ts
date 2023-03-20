1a07aef28d375d1080852513b515bfc2143c8592
simon <moersimonlol123456789@gmail.com>
2023-03-20 09:55:05 +0100

    init tmp project page + root (unfonctionnal)

--- WEB/src/app/app.module.ts
+++ WEB/src/app/app.module.ts
@@ -16,12 +16,15 @@ import { TimelineEvent } from './pages/timelinePage/timelineEvent/timelineEvent.
 import { SignUpPage } from './pages/signUpPage/signUpPage.component';
 import { SignInPage } from './pages/signInPage/signInPage.component';
 
+// import { ProjectPage } from './pages/projectPage/projectPage.component';
+
 import { CookieService } from 'ngx-cookie-service';
 
 const appRoutes: Routes = [
   { path: 'sign-up', component: SignUpPage },
   { path: 'sign-in', component: SignInPage },
   { path: '', component: LandingPage },
+  // { path: 'project', component: ProjectPage }
 ];
 
 @NgModule({
