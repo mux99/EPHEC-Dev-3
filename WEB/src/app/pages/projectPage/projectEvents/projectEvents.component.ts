01e893c3a7ec7cae67caf67f3e06463193a922e5
simon <moersimonlol123456789@gmail.com>
2023-03-23 10:31:17 +0100

    components projectPage folders + files (not work)

---
+++ WEB/src/app/pages/projectPage/projectEvents/projectEvents.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'project-events',
+  styleUrls: ['./projectEvents.component.scss'],
+
+  template: '<div class="eventsdiv"></div>'
+})
+
+export class ProjectEvents { }
