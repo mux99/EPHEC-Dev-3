c00a25b4219a1ff8216fc93b420f65df4b24ae18
maxime <mux99@live.be>
2023-04-27 08:52:57 +0200

    project tot timeline navigation

--- WEB/src/app/pages/projectPage/projectTimeline/projectTimeline.component.ts
+++ WEB/src/app/pages/projectPage/projectTimeline/projectTimeline.component.ts
@@ -1,18 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/shared-services/auth.service';
 
 @Component({
   selector: 'project-timeline',
   styleUrls: ['./projectTimeline.component.scss'],
 
-  template: '<div id="timeline"></div><div id="container"><div #name></div><div #description></div></div>'
+  template: '<div (click)="click()" id="timeline"></div><div id="container"><div #name></div><div #description></div></div>'
 })
 
 export class ProjectTimeline {
   constructor(
     private http: HttpClient,
-    private auth: AuthService
+    private auth: AuthService,
+    private router: Router
   ){}
 
   @Input() timeline_id!: string; project_id!: string;
@@ -26,5 +28,9 @@ export class ProjectTimeline {
       this.name_ref.nativeElement.innerHTML = obs_data.name;
       this.description_ref.nativeElement.innerHTML = obs_data.description;
     })
+  }
+
+  click() {
+    this.router.navigate([`/t/${this.timeline_id}`])
   }
 }
