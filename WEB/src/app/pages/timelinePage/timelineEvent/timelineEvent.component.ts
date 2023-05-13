0bebc6355ba3bb18a75ec07d389162e78bc8b142
maxime <mux99@live.be>
2023-05-13 16:51:29 +0200

    fix: proper event display (on timeline)

--- WEB/src/app/pages/timelinePage/timelineEvent/timelineEvent.component.ts
+++ WEB/src/app/pages/timelinePage/timelineEvent/timelineEvent.component.ts
@@ -1,21 +1,22 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'timeline-event',
   styleUrls: ['./timelineEvent.component.scss'],
 
-  template: '<div></div><p>text</p><event-popup></event-popup>'
+  template: '<div id="img"></div><span #text>text</span>'
 })
 
 export class TimelineEvent {
-  @Input() data!: any; pos!: number; 
+  @Input() data!: any; pos!: number;
 
   @ViewChild("text") text!: ElementRef;
 
   constructor(private elementRef: ElementRef) { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    console.log(this.data);
     this.text.nativeElement.innerHTML = this.data.title;
-    this.elementRef.nativeElement.style.right = `${this.pos}%`
+    this.elementRef.nativeElement.style.left = `${this.pos}%`
   }
 }
--- WEB/src/app/pages/timelinePage/timelinePage.component.ts
+++ WEB/src/app/pages/timelinePage/timelinePage.component.ts
@@ -51,13 +51,12 @@ export class TimelinePage {
       let obs = this.http.get(`/api/projects/${this.project_id}/timelines/${this.timeline_id}`, this.auth.httpHeader);
       obs.subscribe(
         (data: any) => {
-          console.log(data);
           //load timeline data
           this.d_year = data.d_year;
           this.d_month = data.d_month;
 
           //load events
-          for (let i = 0; i < data.events; i++) {
+          for (let i = 0; i < data.events.length; i++) {
             //create host container
             const tmp = this.renderer.createElement('div');
             this.renderer.appendChild(this.events_ref.nativeElement, tmp);
@@ -69,7 +68,9 @@ export class TimelinePage {
             })
             //set elem inputs
             elem.instance.data = data.events[i];
-            elem.instance.pos = (this.datetodays(data.events[i].date)/(this.datetodays(data.stop)-this.datetodays(data.start)))*100
+
+            let tmp2 = this.datetodays(data.events[i].date === undefined ? "0000/00/00" : data.events[i].date);
+            elem.instance.pos = (tmp2/(data.end-data.start))*100;
 
             //add elem to view
             this.applicationRef.attachView(elem.hostView);
@@ -104,12 +105,15 @@ export class TimelinePage {
   }
 
   datetodays(date: string) {
+    console.log(date);
     let date_array = date.split("/").map(function(n,_i,_a){return Number(n);},this);
-    let days = date_array[0]+(date_array[2]*this.d_year);
-    for (let i = 0; i < date_array[1]; i++) {
-      days += this.d_month[i];
-    }
-    return days;
+    console.log(date_array);
+    
+    // let days = date_array[0]+(date_array[2]*this.d_year);
+    // for (let i = 0; i < date_array[1]; i++) {
+    //   days += this.d_month[i];
+    // }
+    return 0;
   }
 
   toggleOptions() {}
