8736b098c04e8fc9badfb0ab7bda76c64e970a43
MIKObscura <105145988+MIKObscura@users.noreply.github.com>
2023-05-31 11:40:57 +0200

    added some controller tests

--- BACKEND/test/controllers/users_controller_test.rb
+++ BACKEND/test/controllers/users_controller_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'post endpoints valid params' do
+    post '/api/users', params: {e: 'stuff@mail.microsoft', n: 'name', p: 'secure_password69'}
+    assert_response :success
+    
+    user = User.create!(name: "stuff", email: "stuff@stuff.stuff", password: "stuff", tag: '0000')
+    post '/api/login', params: {e: 'stuff@stuff.stuff', p: 'stuff'}
+    assert_response :success
+  end
+
+  test 'post endpoints invalid params' do
+    post '/api/users', params: {n: "name", p: "motdepasse"}
+    assert_response 204
+
+    user = User.create!(name: "stuff", email: "stuff@stuff.nope", password: "stuff", tag: '0000')
+    post '/api/login', params: {e: "stuff@stuff.nope", p: 'invalid_password'}
+    assert_response 204
+  end
 end
